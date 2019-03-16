0fe121b2fa97307fef5b4b08ed9b5a203cd1ff39
Kurnia D Win <kurnia.d.win@gmail.com>
2019-03-16 07:31:06 +0700

    inlining some function for performance

--- handler.go
+++ handler.go
@@ -13,38 +13,37 @@ func (c checkT) Error() string {
 // Handle the error, and store it to *errptr, this function must be called on deferred context, i.e. `defer Handle(&err)`.
 // please note that this function adding some overhead. You should use `if err != nil` idiom.
 func Handle(errptr *error) {
-	handler(recover(), func(err error) { *errptr = err })
+	if rec := recover(); rec != nil {
+		if c, ok := rec.(checkT); ok {
+			*errptr = c.err
+		} else {
+			panic(rec)
+		}
+	}
 }
 
 // HandleWith do the same thing as Handle, but call f when error occurs
 func HandleWith(f func(error)) {
-	handler(recover(), f)
-}
-
-func handler(rec interface{}, f func(error)) {
-	if rec == nil {
-		return
-	}
-	if c, ok := rec.(checkT); ok {
-		if f != nil {
+	if rec := recover(); rec != nil {
+		if c, ok := rec.(checkT); ok {
 			f(c.err)
+		} else {
+			panic(rec)
 		}
-	} else {
-		panic(rec)
 	}
 }
 
 // Check the error, will panic if err not nil, it assume that Handle is already deferred.
 func Check(err error) {
-	MustNilOr(err, nil)
+	if err != nil {
+		panic(checkT{err})
+	}
 }
 
 // MustNilOr do the same thing as Check, but call f before panic
-func MustNilOr(err error, f func()) {
+func MustNilOr(err error, f func(error)) {
 	if err != nil {
-		if f != nil {
-			f()
-		}
+		f(err)
 		panic(checkT{err})
 	}
 }
