b97cbd406af5497d9573139218b64c365202dd13
Kurnia D Win <kurnia.d.win@gmail.com>
2021-09-29 11:02:23 +0700

    update

--- doc.go
+++ doc.go
@@ -1,6 +1,8 @@
 // Package errors provide utility for wrapping error.
 //
 // The wrapped error will have more information (stack trace and other error that cause the error).
+//
+// NOTE: v1 is not maintaned anymore, please use v2
 package errors
 
 // DefaultDeep is the default deep when generating stack trace
